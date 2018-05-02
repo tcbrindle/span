d5649e830ccf845f55f478821382c5877df31f8b
Tristan Brindle <t.c.brindle@gmail.com>
2018-05-02 21:23:29 +0100

    Fix pedantic clang warning
    I'm pretty sure the standard says it's okay to specialise a class
    using the struct class-key, but Clang doesn't seem to like it

--- include/tcb/span.hpp
+++ include/tcb/span.hpp
@@ -735,13 +735,14 @@ constexpr auto get(span<E, S> s) -> decltype(s[N])
 namespace std {
 
 template <typename E, ptrdiff_t S>
-struct tuple_size<tcb::span<E, S>> : integral_constant<size_t, S> {};
+class tuple_size<tcb::span<E, S>> : public integral_constant<size_t, S> {};
 
 template <typename E>
 class tuple_size<tcb::span<E, tcb::dynamic_extent>>; // not defined
 
 template <size_t N, typename E, ptrdiff_t S>
-struct tuple_element<N, tcb::span<E, S>> {
+class tuple_element<N, tcb::span<E, S>> {
+public:
     using type = E&;
 };
 
